1e337d37772f5ff3923d5f1f3db912a35cba3ef2
Daniel Crnovcic <danielcrnovcic@yahoo.com>
2018-12-17 16:52:32 +0100

    verzija 5

---
+++ Igralica/src/igralica/dialogs/ObavjestenjaDijalog.java
@@ -0,0 +1,28 @@
+package igralica.dialogs;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class ObavjestenjaDijalog {
+
+	private static void showDialog(AlertType tip, String naslov, String zaglavlje, String poruka) {
+		Alert alert = new Alert(tip);
+		alert.setTitle(naslov);
+		alert.setHeaderText(zaglavlje);
+		alert.setContentText(poruka);
+		alert.showAndWait();
+	}
+
+	public static void showErrorDialog(String naslov, String zaglavlje, String poruka) {
+		showDialog(AlertType.ERROR, naslov, zaglavlje, poruka);
+	}
+
+	public static void showInfoDialog(String naslov, String zaglavlje, String poruka) {
+		showDialog(AlertType.INFORMATION, naslov, zaglavlje, poruka);
+	}
+
+	public static void showWarningDialog(String naslov, String zaglavlje, String poruka) {
+		showDialog(AlertType.WARNING, naslov, zaglavlje, poruka);
+	}
+
+}
